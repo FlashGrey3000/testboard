f2c6dbc17573d467ec604f67601b76b2be984b58
flashgrey <aaftabkhan2005@gmail.com>
2026-02-25 08:56:16 +0530

    fixed time converter and added tests

--- tests/test_time_format_converter.py
+++ tests/test_time_format_converter.py
@@ -1,7 +1,8 @@
 import pytest
+from src.time_format_converter import *
 
 def test_convert_24_to_12():
-    pass
+    assert "11:03 pm" == convert_24_to_12("2303")
 
 def test_convert_12_to_24():
-    pass
+    assert "1205 hrs" == convert_12_to_24("12:05 pm")
